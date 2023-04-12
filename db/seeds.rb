18dd8ada00a07ac3b5e1ce96f60353e646b4a677
Kevin Redmond <kred1150@gmail.com>
2023-04-11 19:27:02 -0500

    Created the seeds file and added in the seeds for all available comeptions in the API.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+competition_response = HTTP.get("https://api.football-data.org/v4/competitions")
+competitions = JSON.parse(competition_response.body)["competitions"]
+
+competitions.each do |competition|
+  Competition.create(
+    external_id: competition["id"],
+    name: competition["name"],
+    code: competition["code"],
+    country_name: competition["area"]["name"],
+    country_code: competition["area"]["code"],
+    country_flag: competition["area"]["flag"],
+    competition_type: competition["type"],
+    competition_logo: competition["emblum"],
+    plan: competition["plan"],
+    last_update: competition["last_updated"],
+  )
+end
