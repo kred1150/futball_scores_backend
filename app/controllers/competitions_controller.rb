0d55d3b88170fd1cef7e39194b46b62979ad740d
Kevin Redmond <kred1150@gmail.com>
2023-04-11 19:15:23 -0500

    Added a require file to test http links. Also created an index function and matching routes in the Competitions controller to show all available competitions.

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -1,2 +1,7 @@
 class CompetitionsController < ApplicationController
+  def index
+    response = HTTP.get("https://api.football-data.org/v4/competitions")
+    sports = JSON.parse(response.body)["competitions"]
+    render json: sports
+  end
 end
