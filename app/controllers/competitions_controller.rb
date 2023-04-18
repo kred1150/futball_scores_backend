093000244a96b388e13da8d84fe41b05f2b8c9e3
Kevin Redmond <kred1150@gmail.com>
2023-04-17 20:11:45 -0400

    added the api key to the rails credentials file. Added a new function in the competitions controller called test to test passing through an X-Auth token along with the get HTTP requests. Added an authenticate funtion as well to authenticate the token before passing it thorugh the test function using the new route for competitions/test. This did not workand the token is being rejected, working on a solution.

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -1,7 +1,29 @@
 class CompetitionsController < ApplicationController
+  include ActionController::HttpAuthentication::Basic::ControllerMethods
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+  @api_key = Rails.application.credentials.futball_api_key
+
+  before_action :authenticate, except: [:index]
+
   def index
     competitions = Competition.all
-    tier_one_competitions = competitions.select { |comp| comp["plan"] == "TIER_ONE" }
-    render json: tier_one_competitions.as_json
+    @tier_one_competitions = competitions.select { |comp| comp["plan"] == "TIER_ONE" }
+    render json: @tier_one_competitions.as_json
+  end
+
+  def test
+    response = HTTP.get("http://api.football-data.org/v4/competitions/PL/standings")
+    # standings = JSON.parse(response.body)
+    render json: response.as_json
+  end
+
+  def authenticate
+    authenticate_or_request_with_http_token do |token, _options|
+      # token_digest = ::Digest::SHA256.hexdigest(token)
+      # api_auth_token_digest = ::Digest::SHA256.hexdigest(api_key)
+      # ActiveSupport::SecurityUtils.secure_compare(token_digest, api_auth_token_digest)
+      token == @api_key
+      ActiveSupport::SecurityUtils.secure_compare(token, TOKEN)
+    end
   end
 end
